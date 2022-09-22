b244d608c7d878ac7e5afd56c8165f16a1e7d5f1
Erik Uus <erik.uus@gmail.com>
2022-09-22 14:35:06 +0300

    Update length.ex

--- playground/lib/str/length.ex
+++ playground/lib/str/length.ex
@@ -1,15 +1,22 @@
 defmodule Str.Length do
   @moduledoc """
-  String Length vs. Content Length
+  This module provides examples of string length vs content length.
+
   Elixir strings are represented internally as a sequence of bytes.
   Special characters take more than one byte to be represented in UTF-8.
   So, the content lenght of string can be greater than number of characters.
   """
 
+  @doc """
+  Returns string length and content length of latin characters.
+  """
   def play_latin do
     get_sizes("abcd")
   end
 
+  @doc """
+  Returns string length and content length of umlaut characters.
+  """
   def play_umlaut do
     get_sizes("õäüö")
   end
