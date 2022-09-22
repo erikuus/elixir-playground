6f6e76b4eeb1ea6901f7fcea90057a8d2a431a8d
Erik Uus <erik.uus@gmail.com>
2022-09-22 14:34:55 +0300

    Simplify map examples

--- playground/lib/map/get.ex
+++ playground/lib/map/get.ex
@@ -1,28 +1,64 @@
 defmodule Map.Get do
   @moduledoc """
-  Accessing Map Values with Atoms vs. Strings
+  This module provides examples of accessing map values with atoms vs strings.
+
   The square-bracket syntax works with all maps.
   When the keys are atoms, you can also use the dot notation.
   """
 
-  def play_good_atom do
-    %{:firstname => "Erik", :lastname => "Uus"}
-    |> get_fullname
+  @doc """
+  Returns fullname using square-bracketed string keys
+  """
+  def play_good do
+    a = %{"firstname" => "Erik", "lastname" => "Uus"}
+    a["firstname"] <> " " <> a["lastname"]
   end
 
-  def play_good_string do
-    %{:firstname => "Erik", :lastname => "Uus"}
-    |> get_fullname
+  @doc """
+  Returns fullname using square-bracketed atom keys
+  """
+  def play_good2 do
+    a = %{:firstname => "Erik", :lastname => "Uus"}
+    a[:firstname] <> " " <> a[:lastname]
   end
 
-  def get_fullname(%{firstname: "Erik", lastname: "Uus"} = map) do
-    map.firstname <> " " <> map[:lastname]
+  @doc """
+  Returns fullname using atoms dot notation
+  """
+  def play_good3 do
+    a = %{:firstname => "Erik", :lastname => "Uus"}
+    a.firstname <> " " <> a.lastname
   end
 
-  def get_fullname(%{"firstname" => "Erik", "lastname" => "Uus"} = map) do
-    map["firstname"] <> " " <> map["lastname"]
+  @doc """
+  Returns nil as a result of using dot notation with string keys.
+  """
+  def play_bad do
+    a = %{"firstname" => "Erik", "lastname" => "Uus"}
+
+    try do
+      a.firstname <> " " <> a.lastname
+    rescue
+      KeyError -> nil
+    end
+  end
+
+  @doc """
+  Returns nil as a result of mixing atom keys with string keys.
+  """
+  def play_bad2 do
+    a = %{:firstname => "Erik", :lastname => "Uus"}
+
+    try do
+      a["firstname"] <> " " <> a["lastname"]
+    rescue
+      ArgumentError -> nil
+    end
   end
 end
 
-IO.inspect(Map.Get.play_good_atom())
-IO.inspect(Map.Get.play_good_string())
+IO.inspect(Map.Get.play_good())
+IO.inspect(Map.Get.play_good2())
+IO.inspect(Map.Get.play_good3())
+IO.inspect(Map.Get.play_bad())
+IO.inspect(Map.Get.play_bad2())
--- playground/lib/map/key.ex
+++ playground/lib/map/key.ex
@@ -1,35 +1,30 @@
 defmodule Map.Key do
   @moduledoc """
-  Using atoms as map keys
-  Elixir atoms are prefixed by a colon:
-  %{ :method => "GET", :path => "/wildthings" }
-  However, it's so common to use atoms as keys:
-  %{ method: "GET", path: "/wildthings" }
-  If the keys are anything but atoms, you must use the general => form:
-  %{ "method" => "GET", "path" => "/wildthings" }
+  This module provides examples of map keys.
   """
 
+  @doc """
+  Returns map where keys are atoms.
+  """
   def play_good do
+    %{firstname: "Erik", lastname: "Uus"}
+  end
+
+  @doc """
+  Returns map where keys are atoms prefixed by a colon.
+  """
+  def play_good2 do
     %{:firstname => "Erik", :lastname => "Uus"}
-    |> pattern_match
   end
 
-  def play_bad do
+  @doc """
+  Returns map where keys are strings.
+  """
+  def play_good3 do
     %{"firstname" => "Erik", "lastname" => "Uus"}
-    |> pattern_match
-  end
-
-  @spec pattern_match(map()) :: map() | String.t()
-  def pattern_match(arg)
-
-  def pattern_match(%{firstname: "Erik", lastname: "Uus"} = map) do
-    map
-  end
-
-  def pattern_match(_map) do
-    "No match"
   end
 end
 
 IO.inspect(Map.Key.play_good())
-IO.inspect(Map.Key.play_bad())
+IO.inspect(Map.Key.play_good2())
+IO.inspect(Map.Key.play_good3())
